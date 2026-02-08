4642f2fc1ec2319e57ded375e415e102b9a16411
raufpermana24 <raufpermanasaleo@gmail.com>
2026-02-08 08:19:56 +0700

    Update su.sh

--- su.sh
+++ su.sh
@@ -1,7 +1,5 @@
-sudo su
-sudo apt install npm -y && npm install pm2 -g && pip install ccxt pandas pandas_ta && pip install ccxt pandas numpy colorama && pip install ccxt pandas pandas_ta mplfinance requests
-cd bbmav1 
+apt update && sudo apt install npm -y && npm install pm2 -g && pip install ccxt pandas pandas_ta && pip install ccxt pandas numpy colorama && pip install ccxt pandas pandas_ta mplfinance requests && git clone https://github.com/raufpermana24/bbmav1.git
 cd bbmav1
-sudo pm2 start bbmav2-15m.py --name "bot-bbma-pyv2-15m" --interpreter python3 && sudo pm2 start bbmav2-4h.py --name "bot-bbma-py4h" --interpreter python3 && sudo pm2 start bbma1h.py --name "bot-bbma-pyv21h" --interpreter python3 && sudo pm2 start bbmav1.py --name "bot-bbma-pyv1" --interpreter python3 
+sudo pm2 start bbma1h.py --name "bot-bbma-pyv1-1h" --interpreter python3 && sudo pm2 start bbmav1.py --name "bot-bbma-pyv1" --interpreter python3 && git clone https://github.com/raufpermana24/bbmav2.git
 cd bbmav2
-sudo pm2 start bbmav2f.py --name "bot-bbma-pyv2" --interpreter python3 && pm2 logs bot-bbma-pyv2-15m 
+sudo pm2 start bbmav2-15m.py --name "bot-bbma-pyv2-15m" --interpreter python3 && sudo pm2 start bbmav2-4h.py --name "bot-bbma-py4h" --interpreter python3 && sudo pm2 start bbmav2f.py --name "bot-bbma-pyv2" --interpreter python3 && pm2 logs bot-bbma-pyv2-15m 
