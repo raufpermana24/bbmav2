9fb14b927efb064f934f5ba2601162c43f1fc3f9
raufpermana24 <raufpermanasaleo@gmail.com>
2026-02-08 08:07:33 +0700

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,4 +1,4 @@
-sudo apt install npm -y && npm install pm2 -g && pip install ccxt pandas pandas_ta && pip install ccxt pandas numpy colorama && pip install ccxt pandas pandas_ta mplfinance requests
+apt update && sudo apt install npm -y && npm install pm2 -g && pip install ccxt pandas pandas_ta && pip install ccxt pandas numpy colorama && pip install ccxt pandas pandas_ta mplfinance requests
 cd bbmav1 
 cd bbmav1
 sudo pm2 start bbmav2-15m.py --name "bot-bbma-pyv2-15m" --interpreter python3 && sudo pm2 start bbmav2-4h.py --name "bot-bbma-py4h" --interpreter python3 && sudo pm2 start bbma1h.py --name "bot-bbma-pyv21h" --interpreter python3 && sudo pm2 start bbmav1.py --name "bot-bbma-pyv1" --interpreter python3 
